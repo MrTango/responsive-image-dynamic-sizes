55d7e5a302d71b371f2e0eff652a2c919966c39e
MrTango <md@derico.de>
2025-04-20 17:46:33 +0300

    add build

---
+++ build/_app/immutable/entry/start.DJ1mq1OY.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/CmyrWXWG.js";export{o as load_css,r as start};
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
-import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
 
 const config = {
 	preprocess: [vitePreprocess(), mdsvex()],
